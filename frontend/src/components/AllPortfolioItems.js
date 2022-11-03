07047b89a266a2d8175826f29c2865336900c920
MrSharipov <84614549+MrSharipov@users.noreply.github.com>
2022-11-03 15:27:36 +0500

    React fetched all data from API

--- frontend/src/components/AllPortfolioItems.js
+++ frontend/src/components/AllPortfolioItems.js
@@ -1,8 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 const AllPortfolioItems = () => {
-    
+    const [items, setItems] = useState([]);
+    useEffect(()=>{
+        const fetchData = async ()=> {
+            try{
+                const res = await axios.get("http://localhost:3003/portfolio");
+                setItems(res.data);
+            }
+            catch(err){
+                console.log(err);
+            }
+        }
+        fetchData()
+    }, []);
+        
   return (
     <>
-  
+      {items.map((item)=>(
+          <div className='card' key={item.id}>
+            <h1>{item.title}</h1>
+            <p>{item.desc}</p>
+          </div>
+        ))}
     </>
   )
 }
